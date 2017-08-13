11e02b6444acd4d6085f7deaf04de79e9b8f9f5c
Andres Gomez Urquiza <andres.gomez@datos.mx>
2017-08-13 14:10:52 -0500

    indentation

--- src/dora/p/buda.clj
+++ src/dora/p/buda.clj
@@ -4,10 +4,11 @@
   (:require [clojure.string :as str]))
 
 (defn buda-api [dataset & args]
-  (let [laurl (url "http://api.datos.gob.mx/v1/" dataset)]
-    (json (slurp (str (if args
-                        (apply assoc laurl args)
-                        laurl))))))
+  (let [laurl (url "http://api.datos.gob.mx/v1/" dataset)
+        first-result (json (slurp (str (if args
+                                         (apply assoc laurl args)
+                                         laurl))))]
+    ))
 
 (defn legible-dataset [m]
   (dissoc (assoc m :tags (str/join "," (map :display-name (:tags m)))
